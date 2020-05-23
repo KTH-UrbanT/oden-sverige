4caaf8dab8bae1607ea1216acf96623941238786
Oleksii Pasichnyi <oleksii.pasichnyi@abe.kth.se>
2020-05-23 15:20:27 +0200

    Updates

---
+++ precache-manifest.65c14648cbf3891914808605359760b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12618f58e7170df25fbcbe9d0ded7686",
+    "url": "/oden-pretty-map/index.html"
+  },
+  {
+    "revision": "5f38fe94385aaa299d51",
+    "url": "/oden-pretty-map/static/css/main.aef24950.chunk.css"
+  },
+  {
+    "revision": "becd41aca1ddccd47dd8",
+    "url": "/oden-pretty-map/static/js/2.4f13a7d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oden-pretty-map/static/js/2.4f13a7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f38fe94385aaa299d51",
+    "url": "/oden-pretty-map/static/js/main.86a91023.chunk.js"
+  },
+  {
+    "revision": "c38ad6e1310b3de6ec1b",
+    "url": "/oden-pretty-map/static/js/runtime-main.649ee344.js"
+  }
+]);
