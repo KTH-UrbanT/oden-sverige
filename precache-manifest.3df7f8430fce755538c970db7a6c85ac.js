ae17d28f3d5767db890acc8d1f1488f6eb0f0b74
Oleksii Pasichnyi <oleksii.pasichnyi@abe.kth.se>
2020-10-24 18:27:53 +0200

    Updates

---
+++ precache-manifest.3df7f8430fce755538c970db7a6c85ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89b06ab81e4e362de9903138ecd7f029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b90c314cbf732f3b6f3e",
+    "url": "/static/css/main.e8e66bfa.chunk.css"
+  },
+  {
+    "revision": "83172f0dc86940916a4d",
+    "url": "/static/js/2.f8c77ea1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8c77ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90c314cbf732f3b6f3e",
+    "url": "/static/js/main.57c52f0c.chunk.js"
+  },
+  {
+    "revision": "b73c3ff8d8f2510d819d",
+    "url": "/static/js/runtime-main.55f8f084.js"
+  }
+]);
