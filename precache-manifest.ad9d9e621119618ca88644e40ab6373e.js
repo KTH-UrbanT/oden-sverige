bb4ca06dbea9dacd98d4554578528032199c9c21
Oleksii Pasichnyi <oleksii.pasichnyi@abe.kth.se>
2020-10-24 18:32:08 +0200

    Updates

---
+++ precache-manifest.ad9d9e621119618ca88644e40ab6373e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a933ac4d637ec68a3ac74ef7d617da31",
+    "url": "/oden-sverige/index.html"
+  },
+  {
+    "revision": "b90c314cbf732f3b6f3e",
+    "url": "/oden-sverige/static/css/main.e8e66bfa.chunk.css"
+  },
+  {
+    "revision": "83172f0dc86940916a4d",
+    "url": "/oden-sverige/static/js/2.f8c77ea1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oden-sverige/static/js/2.f8c77ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90c314cbf732f3b6f3e",
+    "url": "/oden-sverige/static/js/main.57c52f0c.chunk.js"
+  },
+  {
+    "revision": "5d4d08203451591426aa",
+    "url": "/oden-sverige/static/js/runtime-main.63778a35.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3df7f8430fce755538c970db7a6c85ac.js"
+  "/oden-sverige/precache-manifest.ad9d9e621119618ca88644e40ab6373e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/oden-sverige/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
